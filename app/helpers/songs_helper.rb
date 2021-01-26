6426285b0d3e59812697e89917afa6b3af3529dc
Trayshawn <traytwebb14@yahoo.com>
2021-01-25 21:41:37 -0600

    commit tree d2b51c65f1ea644233ebdebb861f9a15b5b373e1

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,20 @@
 module SongsHelper
 
+    # def artist_select(song)
+    #     if song.artist.nil?
+    #         select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name), id="song_artist_id"
+    #     else 
+    #         hidden_field "song[artist_id]", @song.artist_id
+    #     end 
+    # end 
+
+    # # def artist_select(song, path)
+    # #     binding.pry
+    # #     if song.artist && path == "nested"
+    # #       hidden_field_tag "song[artist_id]", song.artist_id
+    # #     else
+    # #       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    # #     end
+    # # end
+
 end
